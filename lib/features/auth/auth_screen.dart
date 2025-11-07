8e85316164b89599d19e9331048409fba3b0973e
mahmoudAhmed122001 <m.lebdah@tocaan.com>
2025-11-07 10:18:57 +0200

    change auth screen

--- lib/features/auth/auth_screen.dart
+++ lib/features/auth/auth_screen.dart
@@ -5,6 +5,13 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Auth'),
+      ),
+      body: const Center(
+        child: Text('Auth'),
+      ),
+    );
   }
 }
